f1bac2595588d422a181754bbfb23f42f7184fa8
judrm13 <168407298+judrm13@users.noreply.github.com>
2025-07-15 12:20:46 +0200

    Update settings.py

--- marker/settings.py
+++ marker/settings.py
@@ -3,3 +3,12 @@ __version_info__ = (2, 1, 5)
 __version__ = ".".join(map(str, __version_info__))
 __author__ = "Mustafa Quraish"
 __license__ = "MIT"
+
+import os
+from pydantic import BaseSettings
+
+class MarkerSettings(BaseSettings):
+    log_level: str = os.getenv("MARKER_LOG_LEVEL", "INFO")
+    # add other configs as needed
+
+settings = MarkerSettings()
